0a13fdd6c60b839064c3c28c5c20f9982254cfd4
Dmytro <predchishindmytro@gmail.com>
2023-09-29 09:06:03 +0300

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3x1, font-bold underline text-red-600'>
+        Simple React Typescript Tailwind Sample
+      </h1>
+    </div>
+  );
+}
+
+export default App;
